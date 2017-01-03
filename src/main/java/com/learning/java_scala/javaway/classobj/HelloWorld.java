4d346d7d5d31da9bc9427bd62e2050acf6664a27
Sasha <skmets@gmail.com>
2017-01-03 02:13:17 +0200

    master

---
+++ src/main/java/com/learning/java_scala/javaway/classobj/HelloWorld.java
@@ -0,0 +1,27 @@
+package com.learning.java_scala.javaway.classobj;
+
+// Java way - say hello world and print date
+
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+
+		String hello = "Hello World!";
+		System.out.println(hello);
+		
+		Date now = new Date();
+		DateFormat df = DateFormat.getDateInstance(DateFormat.LONG, Locale.ENGLISH);
+
+		System.out.println("Hello, world @ " + df.format(now));
+		
+		System.out.println(foo());
+	}
+	
+	public static String foo() {
+		return "Foo";
+	}
+}
