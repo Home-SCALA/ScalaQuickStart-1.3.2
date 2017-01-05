dfe8deb7af74ec45906d455e730359c395ebe361
Sasha <skmets@gmail.com>
2017-01-05 10:35:11 +0200

    master

--- src/test/scala/com/samples/Main1.scala
+++ src/test/scala/com/samples/Main1.scala
@@ -2,26 +2,38 @@ package com.samples
 
 object Main1 {
 
+  // Пример №1:
   class Aaa {
     var name: String = null
     var age: Integer = 0
     def getName() = name
     def getAge() = age
-    override def toString = "[Aaa: name = " + name + ", age = " + age + "]"
+    override def toString = "Aaa(" + name + "," + age + ")"
   }
 
+  // Пример №2:
   class Bbb(val name: String, val age: Integer) {
-    override def toString = "[Bbb: name = " + name + ", age = " + age + "]"
+    override def toString = "Bbb(" + name + "," + age + ")"
   }
+
+  // Пример №3:
+  case class Ccc(val name: String, val age: Integer)
+
 
   def main(args: Array[String]) {
+    // Пример №1:
     val aaa: Aaa = new Aaa()
     aaa.name = "Alex"
     aaa.age = 23
     println( aaa )
 
+    // Пример №2:
     val bbb: Bbb = new Bbb("Alex",23)
     println( bbb )
+
+    // Пример №3:
+    val ccc: Ccc = new Ccc("Alex",23)
+    println( ccc )
   }
 
 }
